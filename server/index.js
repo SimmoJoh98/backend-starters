a395465f04c444e642a559017f9aa961ad3c0186
Xeilon <simmojoh98@gmail.com>
2022-02-07 14:06:29 -0700

    completed lab

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const cors = require('cors')
 app.use(express.json());
 app.use(cors())
 
-app.listen(3001,() => console.log(`Server Listening at 3001`))
 //---------------------------------------------------------------------//
 
 app.get("/api/users", (req,res) => {
@@ -18,3 +17,5 @@ app.get("/weather/:temperature", (req, res) => {
     const phrase = `<h3>It was ${temperature} today</h3>`;
     res.status(200).send(phrase);
 });
+
+app.listen(3001,() => console.log(`Server Listening at 3001`))
